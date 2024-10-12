a1349168af5db85477f816d9f64c6b11b735ce24
safeer85 <msafeer753@gmail.com>
2024-10-12 18:24:06 +0530

    bycrypt

--- Hashpassword.js
+++ Hashpassword.js
@@ -1,4 +1,17 @@
-const bcrypt = require('bcrypt');
+/*const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+const hashPassword = async (password) => {
+    return await bcrypt.hash(password, saltRounds);
+};
+
+(async () => {
+    const hashedPassword = await hashPassword('Ilove8543@');
+    console.log(hashedPassword);  // Use this hashed password when inserting the admin user
+})();*/
+
+
+const bcrypt = require('bcryptjs');
 const saltRounds = 10;
 
 const hashPassword = async (password) => {
